a7ce85d331adf2cf6788135d6865a976e362981a
Saikat Narayan Bhattacharjya <snbhattacharjya@gmail.com>
2020-12-15 09:07:50 +0530

    Added PPMS UI CSS Fonts and JS

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,29 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix
+
+/* CSS */
+.sass('resources/sass/app.scss', 'public/css')
+.sass('resources/sass/appui.scss', 'public/css/ppmsui.css')
+.sass('resources/sass/ppmsui/themes/amethyst.scss', 'public/css/themes/')
+.sass('resources/sass/ppmsui/themes/city.scss', 'public/css/themes/')
+.sass('resources/sass/ppmsui/themes/flat.scss', 'public/css/themes/')
+.sass('resources/sass/ppmsui/themes/modern.scss', 'public/css/themes/')
+.sass('resources/sass/ppmsui/themes/smooth.scss', 'public/css/themes/')
+
+/* JS */
+.js('resources/js/app.js', 'public/js/app.js')
+.js('resources/js/ppmsui/app.js', 'public/js/ppmsui.js')
+
+/* Page JS */
+.js('resources/js/pages/tables_datatables.js', 'public/js/pages/tables_datatables.js')
+
+/* Tools */
+.browserSync('localhost:8000')
+.disableNotifications()
+
+/* Options */
+.options({
+    processCssUrls: false
+});
